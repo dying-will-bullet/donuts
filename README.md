1188a4fe6cc041045c6b2467d800aab72750b857
Ai Hoshino <ambiguous404@gmail.com>
2023-06-09 22:46:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center"> donuts 🍩 </h1>
+
+Create a project using this template and run following command to replace project name.
+
+```Bash
+find . -type f -exec sed -i "s/donuts/<YOUR PROJECT NAME>/g" {} \;
+```
+
+Then [add a license to your project](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/adding-a-license-to-a-repository).
---
+++ examples/build.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+const pkg_name = "donuts";
+const pkg_path = "../src/lib.zig";
+
+const examples = .{
+    "default",
+};
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    inline for (examples) |e| {
+        const example_path = e ++ "/main.zig";
+        const exe_name = "example-" ++ e;
+        const run_name = "run-" ++ e;
+        const run_desc = "Run the " ++ e ++ " example";
+
+        const exe = b.addExecutable(.{
+            .name = exe_name,
+            .root_source_file = .{ .path = example_path },
+            .target = target,
+            .optimize = optimize,
+        });
+        const mod = b.addModule("donuts", .{
+            .source_file = .{ .path = "../src/lib.zig" },
+        });
+        exe.addModule("donuts", mod);
+
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+
+        run_cmd.step.dependOn(b.getInstallStep());
+        const run_step = b.step(run_name, run_desc);
+        run_step.dependOn(&run_cmd.step);
+    }
+}
