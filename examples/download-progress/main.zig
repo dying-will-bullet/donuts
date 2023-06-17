02c811aee70ee8e8df5aef693a5b62f669d22437
Hanaasagi <ambiguous404@gmail.com>
2023-06-17 22:43:39 +0900

    feat: refactor download progress example

--- examples/download-progress/main.zig
+++ examples/download-progress/main.zig
@@ -9,12 +9,15 @@ pub fn main() !void {
     );
 
     try sp.start();
-    std.time.sleep(100 * std.time.ns_per_ms);
+    std.time.sleep(200 * std.time.ns_per_ms);
 
-    var buf: [64]u8 = undefined;
+    var text: [128]u8 = undefined;
     for (0..100) |i| {
-        const text = try std.fmt.bufPrint(&buf, "Progress {d:>02} %", .{i});
-        sp.setMessage(text);
+        const len = (try std.fmt.bufPrint(&text, "Progress {d:>02}% ", .{i})).len;
+        @memset(text[len .. len + i / 2], '#');
+        @memset(text[len + i / 2 ..], '-');
+
+        sp.setMessage(text[0 .. len + 50]);
         std.time.sleep(50 * std.time.ns_per_ms);
     }
     try sp.stop(.{ .symbol = "✅", .message = "Finsihed." });
